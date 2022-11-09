e99d1f2896b9ef23f610e899af364c24a7070428
Lucas <lucasoliveirapedroso@outlook.com>
2022-11-09 00:20:37 -0300

    cria pokemon-model

--- Projeto com HTML CSS para Listagem de Pokémon/assets/js/main.js
+++ Projeto com HTML CSS para Listagem de Pokémon/assets/js/main.js
@@ -1,3 +1,6 @@
+function convertPokemonTypesToLi(pokemonsTypes){
+    return pokemonsTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -7,10 +10,9 @@ function convertPokemonToLi(pokemon) {
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" 
                     alt="${pokemon.name}">
                 </div>
             </li> 
